60182aeb2065b10f02e88abe223f414ec12db8df
VladKuli <goldydefault@gmail.com>
2025-02-20 16:07:38 +0200

    Updating gateway

--- api_gateway/src/main/java/org/example/gateway/security/JwtFilter.java
+++ api_gateway/src/main/java/org/example/gateway/security/JwtFilter.java
@@ -1,37 +1,49 @@
 package org.example.gateway.security;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+
 
 @Component
 public class JwtFilter implements WebFilter {
 
-    private final JwtDecoder jwtDecoder;
-
-    public JwtFilter(JwtDecoder jwtDecoder) {
-        this.jwtDecoder = jwtDecoder;
-    }
+    @Autowired
+    private  ReactiveJwtDecoder jwtDecoder;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
-        if (token != null && token.startsWith("Bearer ")) {
-            token = token.replace("Bearer ", "");
-            try {
-                var jwt = jwtDecoder.decode(token);
-                var authentication = new UsernamePasswordAuthenticationToken(jwt.getSubject(), null, null);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            } catch (JwtException e) {
-                return Mono.empty(); // Invalid JWT, continue without authentication
-            }
+        ServerHttpRequest request = exchange.getRequest();
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            return jwtDecoder.decode(token)
+                    .map(this::createAuthentication)
+                    .flatMap(auth -> chain.filter(exchange)
+                            .contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth)));
         }
+
         return chain.filter(exchange);
     }
+
+    private Authentication createAuthentication(Jwt jwt) {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+        return converter.convert(jwt);
+    }
 }
+
